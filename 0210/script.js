eb49b2f9228531023cebf4e5c4264c03d03f8b5d
Gka2712 <g2172049@tcu.ac.jp>
2024-02-11 12:27:42 +0900

    0211　ブログっぽくね

--- 0210/script.js
+++ 0210/script.js
@@ -1,5 +1,6 @@
 const heartButton=document.getElementById('heartButton');
 const heart=document.querySelector('.heart');
+var buttons=document.querySelectorAll(".scroll");
 let isBlack=true;
 heartButton.addEventListener('click',function(){
     if(isBlack)
@@ -11,4 +12,29 @@ heartButton.addEventListener('click',function(){
         heart.style.backgroundColor='black';
     }
     isBlack=!isBlack;
-})
+});
+buttons.forEach(function(button){
+    button.addEventListener("click",function(){
+        var targetId=this.getAttribute("data-target");
+        var targetElement=document.getElementById(targetId);
+        targetElement.scrollIntoView({behavior:"smooth"}); 
+    });
+});
+window.onscroll=function()
+{
+    var scrollposition=window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop||0;
+    if(scrollPosition>500)
+    {
+        scrollButton.style.display="block";
+    }
+    else
+    {
+        scrollButton.style.display="none";
+    }
+};
+scrollButton.onclick=function(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+};
